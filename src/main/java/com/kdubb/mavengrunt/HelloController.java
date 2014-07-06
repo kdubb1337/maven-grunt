1762c144ae5ed83873733acbea3dd0af95161f1b
Erich Reich <erich.reich@gmail.com>
2014-07-06 16:45:54 -0400

    Initial Commit

---
+++ src/main/java/com/kdubb/mavengrunt/HelloController.java
@@ -0,0 +1,18 @@
+package com.kdubb.mavengrunt;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class HelloController {
+    @RequestMapping(value="/", method=RequestMethod.GET)
+    public String index(Model model) {
+   	 // let�s pass some variables to the view script
+   	 model.addAttribute("wisdom", "Goodbye XML");
+	 		
+   	 // renders /WEB-INF/views/hello.jsp
+   	 return "hello"; 
+    }
+}
